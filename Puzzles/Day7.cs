9c61a6597cd697649d553364f9ccada4a644b47e
Jack DeMeyers <319723@wegmans.com>
2021-12-08 17:48:35 -0500

    day 7

---
+++ Puzzles/Day7.cs
@@ -0,0 +1,60 @@
+﻿namespace AOC2021.Puzzles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Day7 : BasePuzzle
+    {
+        private readonly List<int> _positions;
+
+        public Day7(bool practice)
+            : base(7, practice)
+        {
+            _positions = _puzzleInput.First().Split(",").Select(int.Parse).ToList();
+        }
+
+        public override string Part1Answer => Part1().ToString();
+
+        public override string Part2Answer => Part2().ToString();
+
+        public int Part1()
+        {
+            int minPos = _positions.Min();
+            int maxPos = _positions.Max();
+
+            List<(int position, int fuelCost)> fuelCosts = new();
+            for (int i = minPos; i <= maxPos; i++)
+            {
+                int cost = 0;
+                _positions.ForEach(pos =>
+                {
+                    cost += Math.Abs(pos - i);
+                });
+                fuelCosts.Add((i, cost));
+            }
+            return fuelCosts.Min(x => x.fuelCost);
+        }
+
+        public int Part2()
+        {
+            int minPos = _positions.Min();
+            int maxPos = _positions.Max();
+
+            List<(int position, int fuelCost)> fuelCosts = new();
+            for (int i = minPos; i <= maxPos; i++)
+            {
+                int cost = 0;
+                _positions.ForEach(pos =>
+                {
+                    int moves = Math.Abs(pos - i);
+                    cost += (moves * (moves + 1)) / 2;
+                });
+                fuelCosts.Add((i, cost));
+            }
+            return fuelCosts.Min(x => x.fuelCost);
+        }
+    }
+}
